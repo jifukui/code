ee3b6ed6e0a4ceb50c844cb258cd5ee1b0765bcb
jifukui <805013833@qq.com>
2019-02-27 15:24:46 +0800

    修改tcp_client错误

--- C/tcp_client/client.c
+++ C/tcp_client/client.c
@@ -8,15 +8,15 @@
 #define SERV_PORT 5000
 int main ()
 {
-    struct sockaddr_in servadd;
+    struct sockaddr_in servaddr;
     char buf[MAXLINE];
     int sockfd,n;
     char str[]="#model?\r\n";
     sockfd=socket(AF_INET,SOCK_STREAM,0);
-    bzero(&servaddr,sizeof(servadd));
+    bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family=AF_INET;
     inet_pton(AF_INET,"192.168.20.140",&servaddr.sin_addr);
-    servadd.sin_port=htons(SERV_PORT);
+    servaddr.sin_port=htons(SERV_PORT);
     connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
     write(sockfd,str,strlen(str));
     n=read(socket,buf,MAXLINE);
