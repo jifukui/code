d289d32d439115d47595e42a4f94704557736051
jifukui <805013833@qq.com>
2019-03-27 11:45:48 +0800

    解决冲突

--- C/sscanf/main.c
+++ C/sscanf/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 int main
 {
     int i=0;
@@ -24,20 +23,3 @@ int main
     }
     return 0;
 }
-=======
-int main()
-{
-	char str[30]="12,34,9";
-	char data[3];
-	int status=0;
-	status=sscanf(str,"%d,%d,%d",&data[0],&data[1],&data[2]);
-	printf("The status is %d\n",status);
-	if(status==3)
-	{	
-		printf("The 1 is %d\n",data[0]);
-		printf("The 2 is %d\n",data[1]);
-		printf("The 3 is %d\n",data[3]);
-	}
-	return 0;
-}
->>>>>>> 3f396fdcc78a7f29872275d9a64641e193ee6ad7
