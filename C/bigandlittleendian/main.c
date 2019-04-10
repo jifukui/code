b511bf9ed8ab6a5197a70b676b21ba80422b7e41
jifukui <805013833@qq.com>
2019-04-10 15:24:38 +0800

    大小端宏

---
+++ C/bigandlittleendian/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void memrev16(void *p);
+void memrev32(void *p);
+void memrev64(void *p);
+uint16_t intrev16(uint16_t v);
+uint32_t intrev32(uint32_t v);
+uint64_t intrev64(uint64_t v);
+
+/* variants of the function doing the actual conversion only if the target
+ * host is big endian */
+/**根据大小端的格式设置数据*/
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+#define memrev16ifbe(p) ((void)(0))
+#define memrev32ifbe(p) ((void)(0))
+#define memrev64ifbe(p) ((void)(0))
+#define intrev16ifbe(v) (v)
+#define intrev32ifbe(v) (v)
+#define intrev64ifbe(v) (v)
+#else
+#define memrev16ifbe(p) memrev16(p)
+#define memrev32ifbe(p) memrev32(p)
+#define memrev64ifbe(p) memrev64(p)
+#define intrev16ifbe(v) intrev16(v)
+#define intrev32ifbe(v) intrev32(v)
+#define intrev64ifbe(v) intrev64(v)
+#endif
+int main()
+{
+    char data[1,2,3,4];
+    unsigned int *value=data;
+    printf("The value is %u\n",value);
+    memrev16ifbe(value);
+    printf("The value is %u\n",value);
+    return 0;
+}
