e032fe16f30aec39d0394665a97fb29dec1880ff
jifukui <805013833@qq.com>
2019-10-23 09:15:12 +0800

    添加sqlite3学习

---
+++ C/sqlite3/connect/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "../sqlite3.h"
+int main(int argc,char **argv )
+{
+    sqlite3 *db;
+    char *zErrmsg=null;
+    int rc;
+    rc=sqlite3_open("test.db",&db);
+    if(rc)
+    {
+        fprintf(stderr,"Can't open databases: %s\n",sqlite3_errmsg(db));
+        printf("%s",stderr);
+        exit(0);
+    }
+    else
+    {
+        fprintf(stderr,"opened database successful\n");
+        printf("%s",stderr);
+    }
+    sqlite3_close(db);
+}
