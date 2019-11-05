9275fa6081854272c2f2180324ab7a88141014ae
jifukui <805013833@qq.com>
2019-11-05 16:30:45 +0800

    完成代理

--- web/JavaScript/es6/proxy/proxy.js
+++ web/JavaScript/es6/proxy/proxy.js
@@ -60,13 +60,59 @@ const print=console.log;
             print(value);
         }
     };
-    let value=new Proxy(data,{
-        apply:function(target,data,value){
+    let value=new Proxy(data.ji,{
+        apply:function(target,data,value)
+        {
             print("apply "+value);
+            return "jifukui"
         }
     });
-    //data();
     data.ji(5);
-    let b=7;
-    data.ji.apply(8);
+    let b={a:3,b:9};
+    print(value.call(b,b.b))
+    print(value.apply(b,[b.b]));
+    print(value.bind(b,4)());
+}
+{
+    print("Has的使用");
+    let a={a:1,b:2,c:3};
+    let value=new Proxy(a,{
+        has:function(target,key){
+            print(`The data is ${key}`);
+            return false;
+        }
+    })
+    print('a' in value);
+}
+{
+    print("construct的使用");
+    let value=new Proxy(function(){},{
+        construct:function(targer,args)
+        {
+            print(`args is ${JSON.stringify(args)}`);
+            return {x:1,y:2};
+        }
+    })
+    let a=new value({a:1,b:2});
+    print(JSON.stringify(a));
+    let b=new value([1,2,3]);
+    print(JSON.stringify(b));
+    let c=new value("jifukui nice");
+    print(JSON.stringify(c));
+}
+{
+    print("deleteProperty的使用");
+    let key={
+        name:"guandian",
+        age:12,
+        title:"sb"
+    }
+    let value =new Proxy(key,{
+        deleteProperty:function(target,key)
+        {
+            print(`key is ${key}`);
+            return false;
+        }
+    })
+    print(delete value.age);
 }
