2d2074a498a9fd1e79824f4618700c14824270f6
jifukui <805013833@qq.com>
2019-12-03 17:04:48 +0800

    执行

---
+++ linux/Helloworld/hello.s
@@ -0,0 +1,34 @@
+	.file	"main.c"
+	.section	.rodata
+.LC0:
+	.string	"The data is %s"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	movl	$1819043144, 20(%esp)
+	movl	$1867983983, 24(%esp)
+	movl	$6581362, 28(%esp)
+	leal	20(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$.LC0, (%esp)
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-39)"
+	.section	.note.GNU-stack,"",@progbits
