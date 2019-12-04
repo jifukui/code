53f43c5c6de1003ac167d1d8079410534ee20d9d
jifukui <805013833@qq.com>
2019-12-04 11:31:23 +0800

    添加内存结构的处理

---
+++ linux/guandian/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#define num 100;
+const string str="ddddd";
+int d2=10;
+static int e;
+static int f=5;
+static void func();
+int main()
+{
+    int a=1;
+    float b=1.2;
+    static int c =3;
+    static g;
+    int *p=(int *)malloc(sizeof(int)*1024);
+    return 0;
+}
