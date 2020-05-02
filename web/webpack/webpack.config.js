67c062f553db5a0f0acf5c3edfbfd500312486b7
jifukui <805013833@qq.com>
2020-05-02 10:35:23 +0800

    更新程序

--- web/webpack/webpack.config.js
+++ web/webpack/webpack.config.js
@@ -1,15 +1,19 @@
 const path=require("path");
 //const ExtractTextPlugin=require('extract-text-webpack-plugin');
 module.exports={
+    /**输入文件 */
     entry:{
         utils:'./utils.js',
         audioswitch:'./audioswitch.js'
     },
+    /**输出文件 */
     output:{
         filename:'[name].js',
         path:path.resolve(__dirname,"./dist")
-    }/*,
+    },
+    /**模块 */
     module:{
+        /*
         rules:[
             {
                 test: /\.css$/,
@@ -20,11 +24,14 @@ module.exports={
                     }
                 ]
             }
-        ]
+        ]*/
     },
+    /**插件 */
     plugins:[
-        new ExtractTextPlugin({
+        /*new ExtractTextPlugin({
             filename:'[name]_[contenthash:8].css'
-        })
-    ]*/
+        })*/
+    ],
+    /**服务 */
+    devServer:{}
 }
