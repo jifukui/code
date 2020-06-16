970e4b2927d3a18e4b9741946ca04989a1e44eed
jifukui <805013833@qq.com>
2020-06-16 18:08:44 +0800

    完成zoom球

--- web/Vue实战1/jivue/src/components/D3/zoomball.vue
+++ web/Vue实战1/jivue/src/components/D3/zoomball.vue
@@ -2,33 +2,32 @@
   <div>
     <h3>使用D3的动态球</h3>
     <div id="d3"></div>
+    <input type="button" @click="display" value="显示缩放球" />
   </div>
 </template>
 <script>
 import * as d3 from "d3";
+let radius = 6;
+let theta = Math.PI * (3 - Math.sqrt(5));
+let step = radius * 2;
+let height = 500;
+let width = 500;
+let data = Array.from({ length: 2000 }, (_, i) => {
+  const r = step * Math.sqrt((i += 0.5));
+  const a = theta * i;
+  return [width / 2 + r * Math.cos(a), height / 2 + r * Math.sin(a)];
+});
+let currentTransform = [width / 2, height / 2, height];
 export default {
   data() {
     return {
-      theta: Math.PI * (3 - Math.sqrt(5)),
       radius: 6,
-      step: this.radius * 2,
-      height: 500,
-      width: 500,
-      currentTransform: [this.width / 2, this.height / 2],
+      height,
+      width,
+      currentTransform,
       svg: null,
       g: null,
-      info: { length: 2000 },
-      data: function() {
-        //let length = 2000;
-        Array.from(this.info, (_, i) => {
-          const r = this.step * Math.sqrt((i += 0.5)),
-            a = this.theta * i;
-          return [
-            this.width / 2 + r * Math.cos(a),
-            this.height / 2 + r * Math.sin(a)
-          ];
-        });
-      }
+      data
     };
   },
   methods: {
@@ -47,10 +46,10 @@ export default {
         .data(supper.data)
         .join("circle")
         .attr("cx", function(d) {
-          return supper.data[d].x;
+          return d[0];
         })
         .attr("cy", function(d) {
-          return supper.data[d].y;
+          return d[1];
         })
         .attr("r", function() {
           return supper.radius;
@@ -58,13 +57,39 @@ export default {
         .attr("fill", function(d, i) {
           return d3.interpolateRainbow(i / 360);
         });
+    },
+    step3: function() {
+      let supper = this;
+      let d = this.data[Math.floor(Math.random() * this.data.length)];
+      let i = d3.interpolateZoom(supper.currentTransform, [
+        ...d,
+        radius * 2 + 1
+      ]);
+      this.g
+        .transition()
+        .delay(250)
+        .duration(i.duration)
+        .attrTween("transform", () => t =>
+          supper.step4((supper.currentTransform = i(t)))
+        )
+        .on("end", supper.step3);
+    },
+    step4: function([x, y, r]) {
+      return `
+        translate(${this.width / 2}, ${this.height / 2}) 
+        scale(${this.height / r}) 
+        translate(${-x}, ${-y})  
+      `;
+    },
+    display: function() {
+      console.log(this.data[0]);
+      this.step1();
+      this.step2();
+      this.svg.call(this.step3).node();
     }
   },
   mounted: function() {
     console.log("hello this is zoom ball");
-    this.step1();
-    this.step2();
-    console.log(this.data[0]);
   }
 };
 </script>
