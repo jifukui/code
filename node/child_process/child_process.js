714dd5e098e1bccdff0e792d1655b1cb1752f489
jifukui <805013833@qq.com>
2020-09-15 18:53:26 +0800

    添加node子进程的处理

--- node/child_process/child_process.js
+++ node/child_process/child_process.js
@@ -1,4 +1,5 @@
 const {spawn}=require('child_process');
+//调用子进程
 const ls=spawn('ipconfig',['\/all']);
 ls.stdout.on('data',(data)=>{
     console.log(`stout:${data}`);
@@ -7,6 +8,7 @@ ls.stderr.on('data',(data)=>
 {
     console.error(`stderr ${data}`);
 })
+//监听程序退出以及退出代码
 ls.on('close',(code)=>{
     console.log(`子进程退出程序 ${code}`);
 })
