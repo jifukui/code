dae517327438f7d5667f5c6c2f0271a3caf32f5f
jifukui <805013833@qq.com>
2020-10-13 20:18:58 +0800

    electron的练习

---
+++ web/electron-vue/my-project/src/renderer/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+/*
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})*/
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
---
+++ web/electron-vue/my-project/src/renderer/store/modules/Company.js
@@ -0,0 +1,7 @@
+const company = {
+    name: "北京市利国电子技术有限公司",
+    modelname: "SDVOE管理控制平台"
+}
+export default {
+    state:company
+}
