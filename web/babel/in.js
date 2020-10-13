f4bb9052dfa9698c060eaf15fc45a1b512e5030e
jifukui <805013833@qq.com>
2020-10-13 19:51:39 +0800

    babel的练习

---
+++ web/babel/in.js
@@ -0,0 +1,6 @@
+let arr = new Array(9);
+arr.fill("HDMI");
+console.log(arr);
+let data=()=>{
+    return "dad"
+}
---
+++ web/babel/out.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var arr = new Array(9);
+arr.fill("HDMI");
+console.log(arr);
+var data = function data() {
+    return "dad";
+};
