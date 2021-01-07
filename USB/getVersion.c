ef4cce1d25cae5106f07e81c42ade0e23282e463
guandian <guandian@sohu.com>
2021-01-07 13:52:21 +0800

    添加获取usb的版本

--- USB/getVersion.c
+++ USB/getVersion.c
@@ -1,9 +1,15 @@
 #include <libusb-1.0/libusb.h>
+#include <stdio.h>
 int main(){
     struct libusb_version * version = NULL;
     version = libusb_get_version();
     if(version){
         printf("the major is %d\r\n",version->major);
+        printf("the minor is %d\r\n",version->minor);
+        printf("the micor is %d\r\n",version->micor);
+        printf("the nano is %d\r\n",version->nano);
+        printf("the rc is %s\r\n",version->rc);
+        printf("the describe is %s\r\n",version->describe);
     }else{
         printf("Get Version have error\r\n");
     }
