8af9779c636673c5832e94a68ad6e26cf4efb786
guandian <guandian@sohu.com>
2021-01-07 13:57:19 +0800

    添加获取usb的版本

--- USB/getVersion.c
+++ USB/getVersion.c
@@ -6,7 +6,7 @@ int main(){
     if(version){
         printf("the major is %d\r\n",version->major);
         printf("the minor is %d\r\n",version->minor);
-        printf("the micor is %d\r\n",version->micor);
+        printf("the micro is %d\r\n",version->micro);
         printf("the nano is %d\r\n",version->nano);
         printf("the rc is %s\r\n",version->rc);
         printf("the describe is %s\r\n",version->describe);
