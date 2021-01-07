6b04cd6a36afc4e18d2dee8fe2d47998ca03c24a
guandian <guandian@sohu.com>
2021-01-07 13:27:47 +0800

    添加获取usb的版本

--- USB/getVersion.c
+++ USB/getVersion.c
@@ -3,7 +3,7 @@ int main(){
     struct libusb_version * version = NULL;
     version = libusb_get_version();
     if(version){
-        printf("the major is %d\r\n",version.major);
+        printf("the major is %d\r\n",version->major);
     }else{
         printf("Get Version have error\r\n");
     }
