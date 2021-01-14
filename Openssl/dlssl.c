3a690522e16be4468982740968f9c240c84709ad
jifukui <805013833@qq.com>
2021-01-14 14:57:29 +0800

    动态加载openssl

--- Openssl/dlssl.c
+++ Openssl/dlssl.c
@@ -34,17 +34,17 @@ int main(){
 			printf("have get success %s\r\n",sslfunc[i]);
 		}
 	}
-	SSL_library_init();
-	if ((jifukuictx = SSL_CTX_new(SSLv23_server_method())) == NULL)
-	{
-		printf("load method over\r\n");
-	}
-	else if (SSL_CTX_use_certificate_file(jifukuictx, "thttpd.pem", SSL_FILETYPE_PEM) == 0)
-	{
-		printf("cannot open certificate\r\n");
-	}
-	else if (SSL_CTX_use_PrivateKey_file(jifukuictx, "thttpd.pem", SSL_FILETYPE_PEM) == 0)
-	{
-		printf("cannot open PrivateKey\r\n");
-	}
+	// SSL_library_init();
+	// if ((jifukuictx = SSL_CTX_new(SSLv23_server_method())) == NULL)
+	// {
+	// 	printf("load method over\r\n");
+	// }
+	// else if (SSL_CTX_use_certificate_file(jifukuictx, "thttpd.pem", SSL_FILETYPE_PEM) == 0)
+	// {
+	// 	printf("cannot open certificate\r\n");
+	// }
+	// else if (SSL_CTX_use_PrivateKey_file(jifukuictx, "thttpd.pem", SSL_FILETYPE_PEM) == 0)
+	// {
+	// 	printf("cannot open PrivateKey\r\n");
+	// }
 }
