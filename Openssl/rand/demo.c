2e3aa7866749be97634e69817a0d05f655c4bccc
jifukui <805013833@qq.com>
2021-01-20 14:34:16 +0800

    rand

--- Openssl/rand/demo.c
+++ Openssl/rand/demo.c
@@ -5,5 +5,15 @@
 //#include <openssl/ssl.h>
 int main(){
     printf("the max number is %u\r\n",RAND_MAX);
+    time_t *t;
+    unsigned int value = 0;
+    char data[1024];
+    srand((unsigned int ) time(t));
+    value = rand()/(1024*1024*256);
+    printf("the value is %u\r\n",value);
+    memcpy(data,(const char *)value,1024);
+    for(int i = 0 ; i < 1024 ; i++){
+        printf("the %d value is %c and  %c\r\n",i,data[i],(char *)(value+1));
+    }
     return 0;
 }
