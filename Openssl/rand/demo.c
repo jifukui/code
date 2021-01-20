b505bde2032f7e5817075cbba712272a34c940f5
jifukui <805013833@qq.com>
2021-01-20 14:14:14 +0800

    rand

--- Openssl/rand/demo.c
+++ Openssl/rand/demo.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <openssl/ssl.h>
+//#include <openssl/ssl.h>
 int main(){
     printf("the max number is %u\r\n",RAND_MAX);
     return 0;
