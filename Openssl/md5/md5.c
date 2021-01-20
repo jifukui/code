787cd24daee1d80cca286d610f583771c7bb67d5
jifukui <805013833@qq.com>
2021-01-20 17:29:59 +0800

    md5

--- Openssl/md5/md5.c
+++ Openssl/md5/md5.c
@@ -9,12 +9,12 @@ int main (){
     char in = "amlmdWt1aQ==" ;
     char out [out_len];
     int i ;
-    MD5_CTX * ctx;
+    //MD5_CTX * ctx;
     //EVP_DigestInit_ex(ctx,(const EVP_MD*)EVP_md5(),NULL);
     //EVP_DigestUpdate(ctx,(const void *)in,strlen(in));
     int ret ;
     do{
-        ret = EVP_Digest((const void *)in,strlen(in),out,out_len,ctx,(const EVP_MD*)EVP_md5());
+        ret = EVP_Digest((const void *)in,strlen(in),out,out_len,(const EVP_MD*)EVP_md5());
     }while(ret!=1);
     for(i = 0 ; i < out_len ; i++){
         printf("the data is %02X ",(unsigned char)out[i]);
