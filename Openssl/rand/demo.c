f16f7199c0296801f07c63c089dd91ed23a76f6a
jifukui <805013833@qq.com>
2021-01-20 14:54:07 +0800

    rand

--- Openssl/rand/demo.c
+++ Openssl/rand/demo.c
@@ -10,7 +10,7 @@ int main(){
     char data[64];
     srand((unsigned int ) time(t));
     for(int i = 0 ; i < 64 ; i++ ){
-        data[i] = rand() % 256;
+        data[i] =(unsigned char) (rand() % 128);
         printf("the i is %u and the value is %u\r\n",i,data[i]);
     }
     return 0;
