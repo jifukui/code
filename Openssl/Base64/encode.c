a49cb9b3d6a83c5de5520dcd1917c0726c518f0c
jifukui <805013833@qq.com>
2021-01-20 11:27:51 +0800

    base64

---
+++ Openssl/Base64/encode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <openssl/ssl.h>
+int main(){
+    EVP_ENCODE_CTX *    ectx;
+    char in[]="jifukui",out[100];
+    int outl;
+    EVP_EncodeInit(ectx);
+    EVP_EncodeUpdate(ectx,out,outl,in,strlen(in));
+    //EVP_EncodeFinal(&ectx,out+total,&outl);
+    printf("the encode string is %s and the length is %d\r\n",out,outl);
+    return 0;
+}
