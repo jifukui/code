35f8a37bed674eda6118127bdbbd0eae35c56a1d
jifukui <805013833@qq.com>
2021-01-21 09:49:56 +0800

    sscanf

--- C/sscanf/strInfo.c
+++ C/sscanf/strInfo.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+const int num = 4;
 int main(){
-    char *str[2]={
+    char *str[num]={
         "123456",
-        "jifukui"
+        "jifukui",
+        "ji123ji",
+        "hajhdjh8378-092jhfhy"
     };
     char buf[16];
     char temp[] = "%[^0-9]c";
     int len;
     int i;
-    for( i = 0 ; i < 2 ; i++){
+    for( i = 0 ; i < num ; i++){
         len = sscanf(str[i],temp,buf);
         printf("the %d is %s and match is %d and buf is %s\r\n",i,str[i],len,buf);
     }
