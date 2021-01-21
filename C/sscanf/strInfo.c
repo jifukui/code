6b7687eb77915abdac03c3d6027fba1200d343f7
jifukui <805013833@qq.com>
2021-01-21 09:41:57 +0800

    sscanf

--- C/sscanf/strInfo.c
+++ C/sscanf/strInfo.c
@@ -4,12 +4,13 @@ int main(){
         "123456",
         "jifukui"
     };
+    char buf[16];
     char temp[] = "%[^0-9]c";
     int len;
     int i;
     for( i = 0 ; i < 2 ; i++){
-        len = sscanf(str[i],temp);
-        printf("the %d match is %d\r\n",i,len);
+        len = sscanf(str[i],temp,buf);
+        printf("the %d is %s and match is %d and buf is %s\r\n",i,str[i],len,buf);
     }
     return 0;
 }
