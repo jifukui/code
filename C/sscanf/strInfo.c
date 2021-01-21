630a55e827f86868bf40e2ee477edf0aeda45ee7
jifukui <805013833@qq.com>
2021-01-21 09:23:23 +0800

    sscanf

---
+++ C/sscanf/strInfo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char *str[2]={
+        "123456",
+        "jifukui"
+    };
+    char temp = "%*1[^0-9]";
+    int len;
+    int i;
+    for( i = 0 ; i < 1 ; i++){
+        len = sscanf(str[i],temp);
+        printf("the %d match is %d\r\n",i,len);
+    }
+    return 0;
+}
