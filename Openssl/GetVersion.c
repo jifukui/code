ef8f76de18b56e42906d7bf259ec80fe263c7b8d
jifukui <805013833@qq.com>
2021-02-01 10:56:50 +0800

    openssl version

--- Openssl/GetVersion.c
+++ Openssl/GetVersion.c
@@ -1,6 +1,7 @@
-#include<openssl/ssl.h>
+#include <openssl/ssl.h>
+#include <stdio.h>
 int main(){
-	SSL_library_init();
+	//SSL_library_init();
     printf("the openssl version is %s\r\n",OpenSSL_version(OPENSSL_VERSION));
     return 0;
 }
