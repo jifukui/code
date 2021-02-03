64d3d409b51e87e6c55f79a8b6a1885c98e4a2d3
jifukui <805013833@qq.com>
2021-02-03 16:55:13 +0800

    base64 encode

--- Openssl/Base64/encode.c
+++ Openssl/Base64/encode.c
@@ -3,15 +3,17 @@
 #include <openssl/ssl.h>
 int main(){
     EVP_ENCODE_CTX *    ectx;
-    char in[]="jifukui",out[100];
+    char in[]="jifukui",in1[]="testopenssl",out[100];
     int outl,inl;
     inl = strlen(in);
     printf("the inl len is %d\r\n",inl);
     EVP_EncodeInit(ectx);
     printf("start undate \r\n");
-    //EVP_EncodeUpdate(ectx,out,&outl,in,inl);
-    //printf("input final\r\n");
-    //printf("the encode string is %s and the length is %d\r\n",out,outl);
+    EVP_EncodeUpdate(ectx,out,&outl,in,inl);
+    printf("input final\r\n");
+    EVP_EncodeUpdate(ectx,out,&outl,in1,strlen(in1));
+    printf("input final\r\n");
+    printf("the encode string is %s and the length is %d\r\n",out,outl);
     EVP_EncodeFinal(ectx,out,&outl);
     printf("the encode string is %s and the length is %d\r\n",out,outl);
     return 0;
