a07547009751189c03eabdaa14ef9f8a983d26d7
jifukui <805013833@qq.com>
2021-02-14 17:17:42 +0800

    加载less

--- web/webpack/src/js/main.js
+++ web/webpack/src/js/main.js
@@ -1,11 +1,15 @@
 "use strict";
 import "../css/index.css";
-// import "../less/index.less";
+import "../less/index.less";
 function jifukui(){
     var a = document.getElementById("app");
     var b = document.createElement("div");
+    var c = document.createElement("button");
     var t=document.createTextNode("Hello jifukui");
+    var s=document.createTextNode("click me");
     b.appendChild(t);
+    c.appendChild(s);
     a.appendChild(b);
+    a.appendChild(c);
 }
 jifukui();
