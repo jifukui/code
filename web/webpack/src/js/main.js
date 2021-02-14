b0a3f7af0f0f9ab2804778c702533d5eae653873
jifukui <805013833@qq.com>
2021-02-14 10:15:37 +0800

    css文件的使用

--- web/webpack/src/js/main.js
+++ web/webpack/src/js/main.js
@@ -1,4 +1,11 @@
 "use strict";
+import "../css/index.css";
+// import "../less/index.less";
 function jifukui(){
-    console.log(`this is jifukui study`)
+    var a = document.getElementById("app");
+    var b = document.createElement("div");
+    var t=document.createTextNode("Hello jifukui");
+    b.appendChild(t);
+    a.appendChild(b);
 }
+jifukui();
--- web/webpack/webpack.config.js
+++ web/webpack/webpack.config.js
@@ -19,6 +19,9 @@ module.exports={
             {
                 test:/.js$/,
                 use:"babel-loader"
+            },{
+                test:/.css$/,
+                use:["style-loader","css-loader"]
             }
         ]
     },
