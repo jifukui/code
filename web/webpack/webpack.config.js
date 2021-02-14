10bc0f658993284399fb075023609e3350662157
jifukui <805013833@qq.com>
2021-02-15 07:45:44 +0800

    独立的css文件错误未解决

--- web/webpack/webpack.config.js
+++ web/webpack/webpack.config.js
@@ -1,5 +1,7 @@
 const path=require("path");
 const { webpack } = require("webpack");
+const minicssplugin = require("mini-css-extract-plugin");
+const { loader } = require("mini-css-extract-plugin");
 let home = path.resolve(__dirname);
 console.log(`the home path is ${home}`)
 //const ExtractTextPlugin=require('extract-text-webpack-plugin');
@@ -11,7 +13,7 @@ module.exports={
     },
     /**输出文件 */
     output:{
-        filename:'[name].js',
+        filename:'[name]_[chunkhash:8].js',
         path:path.resolve(home,"dist")
     },
     /**模块 */
@@ -22,28 +24,47 @@ module.exports={
                 use:"babel-loader"
             },{
                 test:/.css$/,
-                use:["style-loader","css-loader"]
+                use:[
+                    "style-loader",
+                    // minicssplugin.loader,
+                    "css-loader"
+                ]
             },{
                 test:/.less$/,
-                use:["style-loader","css-loader","less-loader"]
+                use:[
+                    // "style-loader",
+                    
+                    minicssplugin.loader,
+                    "css-loader",
+                    "less-loader"
+                ]
             },{
                 test:/.(png|gif|jpg|jpeg)$/i,
                 use:["file-loader"]
             },{
                 test:/.(woff)$/i,
-                use:["file-loader"]
+                use:[{
+                    loader:"file-loader",
+                    options:{
+                        name:'[name]_[hash:8].[ext]'
+                    }
+                }]
             }
-        ]
+        ] 
     },
     /**插件 */
     plugins:[
+        new minicssplugin({
+            filename:"[name]_[contenthash:8].css"
+        })
     ],
     /**服务 */
     devServer:{
-        contentBase:"./dist",
-        port:8000,
-        hot:true,
-        hotOnly:true
+        // contentBase:"./dist",
+        // port:8000,
+        // hot:true,
+        // hotOnly:true
     },
-    mode: 'development'
+    // mode: 'development'
+    mode:"production"
 }
