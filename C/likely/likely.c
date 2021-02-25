66b313fc76ff81de66c86c7730c1209c1a6b132c
jifukui <805013833@qq.com>
2021-02-25 16:50:22 +0800

    likely

---
+++ C/likely/likely.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define likely(x) __builtin_expect(!!(x), 1) //x很可能为真       
+#define unlikely(x) __builtin_expect(!!(x), 0) //x很可能为假
+int main(){
+    int i = 0;
+    if(likely(i==0)){
+        printf("is equal\r\n");
+    }else{
+        printf("is not equal\r\n");
+    }
+    return 0;
+}
