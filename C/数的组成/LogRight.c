28ae126fab47678c8410267436d5c353bc26a15c
jifukui <805013833@qq.com>
2021-06-16 16:25:34 +0800

    观察算术右移的区别

--- C/数的组成/LogRight.c
+++ C/数的组成/LogRight.c
@@ -3,9 +3,9 @@ int main(){
     unsigned int a = 4;
     int b = 4;
     int c = -4;
-    a= a>>3;
-    b=b>>3;
-    c =c>>3;
+    a= a>>5;
+    b=b>>5;
+    c =c>>5;
     printf("the a is %d the b is %d the c is %d\r\n",a,b,c);
     return 0;
 }
