0458bb91d7f006d47f7c8e8289077f54378f6ccd
jifukui <805013833@qq.com>
2022-08-30 16:49:23 +0800

    对于 long long 类型的后缀问题

---
+++ c++/c++_new/long long后缀问题/long.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  long long x=65535<<16;
+  long long x1=65535LL<<16;
+  cout<<"the x is "<<x<<endl;
+  cout<<"the x1 is "<<x1<<endl;
+  return 0;
+}
