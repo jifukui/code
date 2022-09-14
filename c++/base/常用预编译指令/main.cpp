13260b381f70c0ac3f9929386e8c430243c1ba1c
jifukui <805013833@qq.com>
2022-09-14 15:12:01 +0800

    添加预编译指令的使用__FILE__ __LINE__ __DATE__ __TIME__

---
+++ c++/base/常用预编译指令/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Hello this is a demo program"<<endl;
+  cout<<"file name is "<<__FILE__<<endl;
+  cout<<"file line is "<<__LINE__<<endl;
+  cout<<"Date is  "<<__DATE__<<endl;
+  cout<<"Time is "<<__TIME__<<endl;
+  return 0;
+}
